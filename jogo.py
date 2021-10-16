362020cc56beab22aa8803950d387383a8d4f33f
Joao S O Bueno <gwidion@gmail.com>
2021-10-16 11:59:52 -0300

    Diminui velocidade

--- jogo.py
+++ jogo.py
@@ -10,15 +10,18 @@ def principal():
     tela = pygame.display.set_mode(TAMANHO)
     x, y = (0, 0)
     vx, vy = 0, 0
+    v = 1 / 3
     while True:
         eventos = pygame.event.get()
         for evento in eventos:
-            if evento.type != pygame.KEYDOWN:
-                continue
-            if evento.key == pygame.K_LEFT: vx = -1
-            if evento.key == pygame.K_RIGHT: vx = 1
-            if evento.key == pygame.K_DOWN: vy = 1
-            if evento.key == pygame.K_UP: vy = -1
+            if evento.type == pygame.KEYDOWN:
+                if evento.key == pygame.K_LEFT: vx = -v
+                if evento.key == pygame.K_RIGHT: vx = v
+                if evento.key == pygame.K_DOWN: vy = v
+                if evento.key == pygame.K_UP: vy = -v
+                if evento.key == pygame.K_ESCAPE: return
+            if evento.type == pygame.KEYUP:
+                vx = vy = 0
         x = x + vx
         y = y + vy
         if x < 0: x= 0
@@ -27,7 +30,7 @@ def principal():
         if y >= h: y = h - 1
 
         tela.fill(FUNDO)
-        pygame.draw.rect(tela, (255, 0, 0), (x * BL , y * BL, BL, BL))
+        pygame.draw.rect(tela, (255, 0, 0), (int(x) * BL , int(y) * BL, BL, BL))
         pygame.display.flip()
         pygame.time.delay(60)
 
