47236229f16f9480490e5fe8a8e640dca4c812a1
Joao S O Bueno <gwidion@gmail.com>
2021-10-16 12:49:25 -0300

    mapa é desenhado

--- jogo.py
+++ jogo.py
@@ -7,6 +7,16 @@ w, h = 16, 12
 BLOCO = BL = W / w
 v = 1 / 3
 
+def lemapa(nome):
+    dados = open(nome).readlines()
+    mapa = {}
+    for y, linha in enumerate(dados):
+        linha = linha.strip("\n")
+        for x, letra in enumerate(linha):
+            mapa[x, y] = letra
+    return mapa
+
+
 def movimento(eventos, x, y, vx, vy):
     for evento in eventos:
         if evento.type == pygame.KEYDOWN:
@@ -25,17 +35,23 @@ def movimento(eventos, x, y, vx, vy):
     if y >= h: y = h - 1
     return x, y, vx, vy
 
-def desenha(tela, personagem, x, y):
+def desenha(tela, personagem, mapa, x, y):
             tela.fill(FUNDO)
+            for mx in range(0, w):
+                for my in range(0, h):
+                    if mapa.get((mx, my)) == "#":
+                        pygame.draw.rect(tela, (255,255, 128), (mx * BL, my * BL, BL, BL))
             tela.blit(personagem, (round(x) * BL , round(y) * BL))
             # pygame.draw.rect(tela, (255, 0, 0), (int(x) * BL , int(y) * BL, BL, BL))
             pygame.display.flip()
+
 
 def principal():
     tela = pygame.display.set_mode(TAMANHO)
     personagem = pygame.image.load("hominho.png")
     escala = 1 / (personagem.get_width() / BL)
     personagem = pygame.transform.rotozoom(personagem, 0, escala)
+    mapa = lemapa("mapa0.txt")
 
     x, y = (0, 0)
     vx, vy = 0, 0
@@ -43,7 +59,7 @@ def principal():
     while True:
         eventos = pygame.event.get()
         x, y, vx, vy = movimento(eventos, x, y, vx, vy)
-        desenha(tela, personagem, x, y)
+        desenha(tela, personagem, mapa, x, y)
         pygame.time.delay(60)
 
 try:
